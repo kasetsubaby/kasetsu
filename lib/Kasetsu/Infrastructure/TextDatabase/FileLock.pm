7d85154b712e48769513ca423b2acb742363a82c
mp0liiu <raian@reeshome.org>
2020-02-05 14:59:03 +0900

    filelock の実装

--- lib/Kasetsu/Infrastructure/TextDatabase/FileLock.pm
+++ lib/Kasetsu/Infrastructure/TextDatabase/FileLock.pm
@@ -1,52 +1,126 @@
 package Kasetsu::Infrastructure::TextDatabase::FileLock;
-use strict;
-use warnings;
-use utf8;
+use Kasetsu::Base;
+use Mouse;
+use Types::Standard qw( InstanceOf ArrayRef Str );
+use namespace::autoclean;
+
+use List::Util qw( any );
+use Path::Tiny qw( path );
+
+has path_str => (
+  is       => 'ro',
+  isa      => Str,
+  init_arg => 'path',
+  required => 1,
+);
+
+has lines => (
+  is       => 'ro',
+  isa      => ArrayRef[Str],
+  required => 1,
+);
+
+has path => (
+  is       => 'ro',
+  isa      => InstanceOf['Path::Tiny'],
+  lazy     => 1,
+  default  => sub {
+    my $self = shift;
+    path($self->path_str);
+  },
+  init_arg => undef,
+);
+
+has lockfile_path => (
+  is      => 'ro',
+  isa     => InstanceOf['Path::Tiny'],
+  lazy    => 1,
+  default => sub {
+    my $self = shift;
+    $self->path->parent->child($self->path->basename . '.tmp');
+  },
+);
+
+has tmpfile_path => (
+  is      => 'ro',
+  isa     => InstanceOf['Path::Tiny'],
+  lazy    => 1,
+  default => sub {
+    my $self = shift;
+    $self->path->parent->child($self->path->basename . '.dmy.tmp');
+  },
+);
+
+sub tmpfiles {
+  my $self = shift;
+  [ $self->lockfile_path, $self->tmpfile_path ];
+}
+
+sub is_locking {
+  my $self = shift;
+  my @tmpfiles = $self->tmpfiles->@*;
+  my @remains_files =
+    grep {
+      my $path = $_;
+      any { $_ eq $path } @tmpfiles;
+    }
+    $self->path->parent->children;
+
+  return !!0 unless @remains_files;
+
+  my $mtime   = $self->lockfile_path->exists ? $self->lockfile_path->stat->mtime : 0;
+  my $at_last = time - 10 - $mtime;
+  if ($mtime && 0 < $at_last) {
+    $self->unlock();
+    0;
+  }
+  else {
+    1;
+  }
+}
 
 sub save_data {
-  # $datadir = $CHARA_DATA | $LOG_DIR
-  my ($filename, $data, $datadir) = @_;
+  my $self = shift;
 
-  my $tmp_filename       = $filename . '.tmp';
-  my $tmp_dummy_filename = $filename . '.dmy.tmp';
-  my $file               = substr($filename, length($datadir) + 1);
-
-  opendir(my $dh, $datadir);
-  my @filenames = readdir $dh;
-  closedir $dh;
-
-  for my $filename (@filenames) {
-    if ($filename =~ /($file).*\.tmp$/) {
-      my $mtime   = (stat $tmp_filename)[9];
-      my $at_last = time - 60 - $mtime;
-      unlock() if $mtime && 0 < $at_last;
-      die "テンポラリファイルが存在します。ロック解除まで $at_last 秒";
-    }
-  }
+  die "ファイルがロックされています。" if $self->is_locking();
 
   eval {
-    open my $tmp, '>', $tmp_filename or die "テンポラリファイルが作成出来ません。($!)";
-    $tmp->close or die "テンポラリファイルがクローズ出来ません。($!)";
-
-    open my $dummy, '>', $tmp_dummy_filename or die "格納用一時ファイルが作成出来ません。($!)";
-    $dummy->close or die '';
-
-    chmod 0666, $tmp_dummy_filename or die $!;
-
+    my ($lockfile_path, $tmpfile_path) = ($self->lockfile_path, $self->tmpfile_path);
+    $lockfile_path->touch();
+    $tmpfile_path->touch();
+    $tmpfile_path->chmod(0666);
+    $tmpfile_path->spew($self->lines->@*);
+    $tmpfile_path->move($self->path);
+    $lockfile_path->remove();
   };
-  unlock() if $@;
-
-  open my $dummy, '>', $tmp_dummy_filename or die "格納用一時ファイルがオープン出来ません。($!)";
-  $dummy->print(@$data);
-  $dummy->close or die $!;
-
-  rename $tmp_dummy_filename, $filename or die $!;
-  unlink $tmp_filename or die $!;
+  if (my $e = $@) {
+    $self->unlock();
+    die $e;
+  }
 }
 
 sub unlock {
-  my ($tmp_filename, $tmp_dummy_filename) = @_;
-  unlink $_ for ($tmp_filename, $tmp_dummy_filename);
+  my $self = shift;
+  for my $path ($self->tmpfiles->@*) {
+    $path->remove();
+  }
 }
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+  Kasetsu::Infrastructure::TextDatabase::FileLock;
+
+=head1 DESCRIPTION
+
+  ファイルにロックをかけつつ保存する機能を提供するモジュールです。
+  元のコードの suport.pl のファイル保存&ロック機能(sub SAVE_DATA) と同等の機能をそなえ、また同時に運用してもデータロストが発生しないような仕組みになっています。
+
+  元のコードを利用している部分がなくなったのであれば、 flock を利用するように実装を変えるべきだと思います。
+
+=cut
--- memo.md
+++ memo.md
@@ -1,3 +1,4 @@
+- filelock は　suport.pl の機構を真似る
 - decoder, encoder で使っているテストクラスをまとめる
 - DTOクラスの作成, エラーのチェックがちゃんとされないので大変そう
   - 例えば NestedColumn なのに isa が InstanceOf[A] でないとか
