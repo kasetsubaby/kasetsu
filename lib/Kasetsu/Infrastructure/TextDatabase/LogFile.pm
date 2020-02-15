a836d16cbc462958a048197a451d475a6b0952cc
mp0liiu <raian@reeshome.org>
2020-02-15 17:54:27 +0900

    Write test for LogFile

--- lib/Kasetsu/Infrastructure/TextDatabase/LogFile.pm
+++ lib/Kasetsu/Infrastructure/TextDatabase/LogFile.pm
@@ -4,46 +4,56 @@ use Mouse;
 use namespace::autoclean;
 BEGIN { extends 'Kasetsu::Infrastructure::TextDatabase::File' }
 
-use Type::Params qw( compile Invocant );
-use Types::Standard qw( Int ArrayRef InstanceOf );
-use Fcntl qw( :flock );
 use Encode qw( encode_utf8 decode_utf8 );
 use File::ReadBackwards;
 
 sub fetch_rows {
   state $c = compile(Invocant, Int);
   my ($self, $num) = $c->(@_);
+
   my $bw = File::ReadBackwards->new($self->path);
   my @rows = map {
     my $line = $bw->readline;
+    chomp $line;
     $self->decoder->decode(decode_utf8 $line);
   } 0 .. $num - 1;
+  $bw->close;
   \@rows;
 }
 
 sub fetch_all_rows {
   my $self = shift;
-  open my $fh, '<', $self->path or die $!;
-  my @rows = map { $self->decoder->decode(decode_utf8 $_) } <$fh>;
+  my $bw = File::ReadBackwards->new($self->path);
+  my @rows;
+  while ( my $line = $bw->readline ) {
+    chomp $line;
+    push @rows, $self->decoder->decode(decode_utf8 $line);
+  }
+  $bw->close;
   \@rows;
 }
 
 sub append_row {
-  state %checker_of_dto_class;
-  $checker_of_dto_class{ $_[0]->dto_class } //= compile(Invocant, InstanceOf[ $_[0]->dto_class ]);
-  my ($self, $row) = $checker_of_dto_class{ $_[0]->dto_class }->(@_);
+  my $self = shift;
+  state %validator_of_dto_class;
+  $validator_of_dto_class{ $self->dto_class } //= compile(InstanceOf[ $self->dto_class ]);
+  my ($row) = $validator_of_dto_class{ $self->dto_class }->(@_);
 
-  my $line = encode_utf8( $self->encoder->encode($row) );
+  my $line = encode_utf8( $self->encoder->encode($row) ) . "\n";
   open my $fh, '>>', $self->path or die $!;
   $fh->print($line);
 }
 
 sub append_rows {
-  state %checker_of_dto_class;
-  $checker_of_dto_class{ $_[0]->dto_class } //= compile(Invocant, ArrayRef[ InstanceOf[ $_[0]->dto_class ] ]);
-  my ($self, $rows) = $checker_of_dto_class{ $_[0]->dto_class }->(@_);
+  my $self = shift;
+  state %validator_of_dto_class;
+  $validator_of_dto_class{ $self->dto_class } //=
+    compile(ArrayRef[ InstanceOf[ $self->dto_class ] ]);
+  my ($rows) = $validator_of_dto_class{ $self->dto_class }->(@_);
 
-  my @lines = map { encode_utf8( $self->encoder->encode($_) ) } @$rows;
+  my @lines =
+    map { "$_\n" }
+    map { encode_utf8( $self->encoder->encode($_) ) } @$rows;
   open my $fh, '>>', $self->path or die $!;
   $fh->print(@lines);
 }
---
+++ t/Infrastructure/TextDataBase/log_file.t
@@ -0,0 +1,75 @@
+use Kasetsu::Base;
+use Test2::V0;
+
+use File::Temp;
+use aliased 'Kasetsu::Infrastructure::TextDatabase::LogFile';
+use aliased 'Kasetsu::Infrastructure::TextDatabase::Column';
+use aliased 'Kasetsu::Infrastructure::TextDatabase::Columns';
+
+package Row {
+  use Kasetsu::Base;
+  use Mouse;
+
+  has a => (
+    is        => 'ro',
+    isa       => Int,
+    required  => 1,
+  );
+
+  has b => (
+    is        => 'ro',
+    isa       => Int,
+    required  => 1,
+  );
+
+  __PACKAGE__->meta->make_immutable;
+}
+
+my %logfile_params = (
+  dto_class => 'Row',
+  columns   => Columns->new(
+    contents => [
+      Column->new(
+        name            => 'a',
+        access_control  => 'ro',
+        type_constraint => Int,
+      ),
+      Column->new(
+        name            => 'b',
+        access_control  => 'ro',
+        type_constraint => Int,
+      ),
+    ],
+  ),
+);
+
+subtest 'append_row and fetch_rows' => sub {
+  my $file = LogFile->new(
+    path => File::Temp->new->filename,
+    %logfile_params,
+  );
+
+  my $row = Row->new(a => 5, b => 2);
+  $file->append_row($row);
+
+  my $row2 = Row->new(a => 0, b => 0);
+  $file->append_row($row2);
+
+  my $fetch_rows = $file->fetch_rows(2);
+  is [ $row2, $row ], $fetch_rows;
+};
+
+subtest 'append_rows and fetch_all_rows' => sub {
+  my $file = LogFile->new(
+    path => File::Temp->new->filename,
+    %logfile_params,
+  );
+
+  my @rows = map { Row->new(a => $_, b => $_ * 2) } 1 .. 5;
+  $file->append_rows(\@rows);
+
+  my $fetch_rows = $file->fetch_all_rows();
+  is [ reverse @rows ], $fetch_rows, '最後に追加した行から順番に取り出す';
+};
+
+done_testing;
